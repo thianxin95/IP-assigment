2a4569d5ab5a68e0810abf5141ef96a3b4b4a931
User <User@192.168.0.4>
2018-08-04 22:26:54 +0800

    Added Validation

---
+++ Function/CatalogValidation.php
@@ -0,0 +1,53 @@
+<?php
+
+$servername = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_table = "fioredb";
+
+class CatalogValidation {
+    
+    private $_db;
+    static $_instance;
+    
+    function __construct() {
+        $this->_db = new PDO('mysql:host=localhost;dbname=fioredb', 'root', '');
+        $this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+    private static function getInstance() {
+        if (!(self::$_instance instanceof self)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+    
+    private function query($sql) {
+        return $this->_db->query($sql);
+    }
+    
+    private function close(){
+        self::$_instance = null;
+    }
+    
+    public function checkProductCode($productCode){
+        $sql = "SELECT productCode FROM product WHERE productCode = '$productCode'";
+        $conn = CatalogValidation::getInstance();
+        $productResult = $conn->query($sql);
+        if(!$productResult){
+            trigger_error("Invalid query: " . $conn->error);
+        }
+        $conn->close();
+        
+        $row = $productResult->fetch(PDO::FETCH_ASSOC);
+        $productCode = $row["productCode"];
+        
+        $result = $productCode; 
+        
+        return $result;
+        
+        }
+        
+        }
+        
+        ?>
