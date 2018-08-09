7b1739314a9dfcf5b79b18618944afbf039bfc9a
ching shean <ching shean@chingshean-PC>
2018-08-09 15:51:22 +0800

    updated

--- CustomerSite/InvoiceController.php
+++ CustomerSite/InvoiceController.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ 
+ *
+ * @author Chang kwok Fei
+ */
 
 
 include_once 'Object/CustomerOb.php';
