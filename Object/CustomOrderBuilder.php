28405e59048b55323ca8971592a30f35fd754028
Hibiki <Hibiki@MSI-Pc>
2018-08-09 21:23:55 +0800

    recover builder getter

--- Object/CustomOrderBuilder.php
+++ Object/CustomOrderBuilder.php
@@ -28,6 +28,34 @@ class CustomOrderBuilder implements Builder{
 
     }
 
+    public function getCustOrderID() {
+        return $this->custOrderID;
+    }
+
+    public function getUserID() {
+        return $this->userID;
+    }
+
+    public function getPickup() {
+        return $this->pickup;
+    }
+
+    public function getDeliveryAdd() {
+        return $this->deliveryAdd;
+    }
+
+    public function getRequireDate() {
+        return $this->requireDate;
+    }
+
+    public function getTotalAmt() {
+        return $this->totalAmt;
+    }
+
+    public function getPaymentStatus() {
+        return $this->paymentStatus;
+    }
+
     public function custOrderID($custOrderID) {
         $this->custOrderID = $custOrderID;
         return $this;
